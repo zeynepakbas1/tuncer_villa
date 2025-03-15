dde2f99970613ee7ff24032ecacd7c2c112aca60
zeynepakbas1 <akbaszeyno@hotmail.com>
2025-03-15 13:40:11 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,511 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gr-vestyer",
+      "name": "GİRİŞ VESTİYER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 1.6090520172590335,
+        "pitch": 0.05978396856676227,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5824825859995766,
+          "pitch": 0.12476542652386158,
+          "rotation": 0,
+          "target": "1-ana-hol"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ana-hol",
+      "name": "ANA HOL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5652762628827475,
+          "pitch": 0.16720822293096482,
+          "rotation": 0,
+          "target": "0-gr-vestyer"
+        },
+        {
+          "yaw": 0.07718609313097602,
+          "pitch": 0.08933270121794834,
+          "rotation": 0,
+          "target": "6-hasan-bey-antre"
+        },
+        {
+          "yaw": 3.092809286380896,
+          "pitch": 0.14049611669808115,
+          "rotation": 0,
+          "target": "7-sedat-bey-antre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sedat-bey-mutfak",
+      "name": "SEDAT BEY MUTFAK",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5135832967220768,
+          "pitch": 0.0365714012245455,
+          "rotation": 0,
+          "target": "7-sedat-bey-antre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sedat-bey-salon",
+      "name": "SEDAT BEY SALON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.009039690274136,
+          "pitch": 0.054907671875334074,
+          "rotation": 0,
+          "target": "7-sedat-bey-antre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hasan-bey-salon",
+      "name": "HASAN BEY SALON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1481299782359455,
+          "pitch": 0.04782205627227576,
+          "rotation": 0,
+          "target": "6-hasan-bey-antre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hasan-bey-mutfak",
+      "name": "HASAN BEY MUTFAK",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5814349346584304,
+          "pitch": 0.030954726214385886,
+          "rotation": 0,
+          "target": "6-hasan-bey-antre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-hasan-bey-antre",
+      "name": "HASAN BEY ANTRE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.558570041596411,
+          "pitch": 0.09079659511612448,
+          "rotation": 0,
+          "target": "1-ana-hol"
+        },
+        {
+          "yaw": -1.7063502612740677,
+          "pitch": 0.08560961168219805,
+          "rotation": 0,
+          "target": "5-hasan-bey-mutfak"
+        },
+        {
+          "yaw": -0.6844188788826742,
+          "pitch": 0.10567243549848548,
+          "rotation": 0,
+          "target": "4-hasan-bey-salon"
+        },
+        {
+          "yaw": 0.7664689615591787,
+          "pitch": 0.08662325515080482,
+          "rotation": 0,
+          "target": "9-hasan-bey-msafr-yatak-odasi"
+        },
+        {
+          "yaw": 1.554931982351678,
+          "pitch": 0.14402569476651195,
+          "rotation": 1.5707963267948966,
+          "target": "8-hasan-bey-ebeveyn-yatak-odasi"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sedat-bey-antre",
+      "name": "SEDAT BEY ANTRE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18065414022408888,
+          "pitch": 0.11257465427962998,
+          "rotation": 0,
+          "target": "1-ana-hol"
+        },
+        {
+          "yaw": -1.4767429273490542,
+          "pitch": 0.1587543560418112,
+          "rotation": 0,
+          "target": "2-sedat-bey-mutfak"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-hasan-bey-ebeveyn-yatak-odasi",
+      "name": "HASAN BEY EBEVEYN YATAK ODASI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2948705797604987,
+          "pitch": 0.08618524625584456,
+          "rotation": 0,
+          "target": "6-hasan-bey-antre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-hasan-bey-msafr-yatak-odasi",
+      "name": "HASAN BEY MİSAFİR YATAK ODASI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": -1.5559072620859133,
+        "pitch": 0.035870381140075125,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7521044546841438,
+          "pitch": 0.08347554641828303,
+          "rotation": 0,
+          "target": "6-hasan-bey-antre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-sedat-bey-oturma-odasi",
+      "name": "SEDAT BEY OTURMA ODASI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.44153224553690507,
+          "pitch": 0.10777979967772566,
+          "rotation": 0,
+          "target": "7-sedat-bey-antre"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "TUNCER VİLLA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
